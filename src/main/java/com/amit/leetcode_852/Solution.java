04bbe17da77fcc4fa42d2ee7d30fcdffc5af9c27
Amit Mondal <amit.kuet@gmail.com>
2020-05-30 00:44:07 +0200

    Solutions with Java

--- src/main/java/com/amit/leetcode_852/Solution.java
+++ src/main/java/com/amit/leetcode_852/Solution.java
@@ -2,6 +2,21 @@ package com.amit.leetcode_852;
 
 public class Solution {
 
+	public int peakIndexInMountainArrayOptimal(int[] A) {
+		int left = 0;
+		int right = A.length - 1;
+
+		while (left < right) {
+			int mid = left + (right - left) / 2;
+			if (A[mid + 1] > A[mid]) {
+				left = mid + 1;
+			} else {
+				right = mid;
+			}
+		}
+		return left;
+	}
+
 	public int peakIndexInMountainArray(int[] A) {
 		if (A == null || A.length == 0) {
 			return 0;
@@ -15,8 +30,11 @@ public class Solution {
 	}
 
 	public static void main(String[] args) {
-		System.out.println(new Solution().peakIndexInMountainArray(new int[] {0, 1, 0})); // Output is 1
-		System.out.println(new Solution().peakIndexInMountainArray(new int[] {0, 2, 1, 0})); // Output is 1
+		System.out.println(new Solution().peakIndexInMountainArray(new int[] { 0, 1, 0 })); // Output is 1
+		System.out.println(new Solution().peakIndexInMountainArray(new int[] { 0, 2, 1, 0 })); // Output is 1
+		
+		System.out.println(new Solution().peakIndexInMountainArrayOptimal(new int[] { 0, 1, 0 })); // Output is 1
+		System.out.println(new Solution().peakIndexInMountainArrayOptimal(new int[] { 0, 2, 1, 0 })); // Output is 1
 	}
 
 }
